034463befd01ed69b32e1f03117539e0669a2eb4
Mudasir Issah <danladi496@gmail.com>
2022-11-02 13:22:11 +0000

    fixed-error-when-lint-is-disabled-inclasess

--- src/modules/Book.js
+++ src/modules/Book.js
@@ -1,20 +1,16 @@
-/* eslint-disable */
-
 export default class Book {
   constructor(books) {
     this.books = books;
   }
 
-  get() {
-    return this.books;
-  }
+  get = () => this.books
 
-  update() {
+  update = () => {
     this.books = JSON.parse(localStorage.getItem('books'));
   }
 
   // method to add book by geting data from dom
-  addBook(e) {
+  addBook = (e) => {
     const booksShow = document.querySelector('.books-show');
     booksShow.style.display = 'block';
     const title = document.getElementById('title').value;
@@ -32,54 +28,51 @@ export default class Book {
 
   // show list after add book func
   // select the nav items
-  showList() {
-    const list = document.getElementById('list');
-    const addnew = document.getElementById('addnew');
-    const contact = document.getElementById('contact');
-    const addbook = document.querySelector('.area');
-    const showbook = document.querySelector('.show-book-list');
-    const contactme = document.querySelector('.contact-me');
-    showbook.style.display = 'flex';
-    addbook.style.display = 'none';
-    contactme.style.display = 'none';
-  }
-  // method to create books and append to dom
+   showList = () => {
+     const addbook = document.querySelector('.area');
+     const showbook = document.querySelector('.show-book-list');
+     const contactme = document.querySelector('.contact-me');
+     showbook.style.display = 'flex';
+     addbook.style.display = 'none';
+     contactme.style.display = 'none';
+   }
+   // method to create books and append to dom
 
-  createBookFromLocal() {
-    for (let i = 0; i < this.books.length; i += 1) {
-      const booksShow = document.querySelector('.books-show');
-      booksShow.style.display = 'block';
-      const book = this.bookToDom(this.books[i].title, this.books[i].author);
-      booksShow.appendChild(book);
-    }
-  }
+   createBookFromLocal = () => {
+     for (let i = 0; i < this.books.length; i += 1) {
+       const booksShow = document.querySelector('.books-show');
+       booksShow.style.display = 'block';
+       const book = this.bookToDom(this.books[i].title, this.books[i].author);
+       booksShow.appendChild(book);
+     }
+   }
 
-  bookToDom(title, author) {
-    const book = document.createElement('li');
-    book.setAttribute('title', title);
-    const p = document.createElement('p');
-    p.innerHTML = `${title} by ${author}`;
-    book.append(p);
-    const remove = document.createElement('button');
-    remove.innerText = 'remove';
-    remove.addEventListener('click', () => {
-      this.remove(title);
-    });
-    book.append(remove);
-    return book;
-  }
-  // method to remove li by getting title
+   bookToDom = (title, author) => {
+     const book = document.createElement('li');
+     book.setAttribute('title', title);
+     const p = document.createElement('p');
+     p.innerHTML = `${title} by ${author}`;
+     book.append(p);
+     const remove = document.createElement('button');
+     remove.innerText = 'remove';
+     remove.addEventListener('click', () => {
+       this.remove(title);
+     });
+     book.append(remove);
+     return book;
+   }
+   // method to remove li by getting title
 
-  remove(title) {
-    const removedBooks = this.books.filter((item) => item.title !== title);
-    localStorage.setItem('books', JSON.stringify(removedBooks));
-    const removedBook = document.querySelector(`li[title='${title}']`);
-    removedBook.remove();
-    this.update();
-    removedBook.style.display = 'none';
-    if (this.books.length === 0) {
-      const booksShow = document.querySelector('.books-show');
-      booksShow.style.display = 'none';
-    }
-  }
+   remove = (title) => {
+     const removedBooks = this.books.filter((item) => item.title !== title);
+     localStorage.setItem('books', JSON.stringify(removedBooks));
+     const removedBook = document.querySelector(`li[title='${title}']`);
+     removedBook.remove();
+     this.update();
+     removedBook.style.display = 'none';
+     if (this.books.length === 0) {
+       const booksShow = document.querySelector('.books-show');
+       booksShow.style.display = 'none';
+     }
+   }
 }
